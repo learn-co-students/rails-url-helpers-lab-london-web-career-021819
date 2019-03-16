5643f99eee3cce385392bc2da4e3e92f7552c318
CalieR <calielrushton@gmail.com>
2019-03-16 12:46:42 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,21 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
+
+
   
   def index
     @students = Student.all
   end
 
-  def show
+  def activate_student_path
+    @student.toggle(:active)
   end
+  # needs a separate view??
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+    # @student used in show page (individual student)
+  def set_student
+    @student = Student.find(params[:id])
+  end
 end
